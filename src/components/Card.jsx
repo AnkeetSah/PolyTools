5601d20677ac9a7faef35fdbfff64c2441cfe7ff
AnkeetSah <ankeetkumar7777@gmail.com>
2025-05-24 21:00:15 +0530

    Solving some link issues1

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,12 +6,12 @@ const Card = ({ title, desc, link }) => {
         <div className="rounded-xl bg-white p-6 shadow-md hover:shadow-lg">
             <h3 className="text-lg xs:text-xl mb-1  lg:font-medium xs:font-bold">{title}</h3>
             <p className="text-gray-400  lg:text-sm xs:text-lg">{desc}</p>
-            <a
-                href={link}
+            <NavLink
+                to={link}
                 className="inline-flex items-center text-purple-600 text-sm font-medium mt-3"
             >
                 Try Now
-            </a>
+            </NavLink>
         </div>
     );
 };
